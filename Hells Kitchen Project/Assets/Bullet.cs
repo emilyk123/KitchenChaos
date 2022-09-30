155c16132130eb280137687af2024d1a22587427
KornTortilla <ryanagundez@gmail.com>
2022-09-29 21:06:33 -0700

    Added Player Movement and Shooting

---
+++ Hells Kitchen Project/Assets/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector2 dir;
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = dir * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Hells Kitchen Project/Assets/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+
+    private Vector2 movement;
+
+    public GameObject bullet;
+
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject newBullet = GameObject.Instantiate(bullet, rb.position, Quaternion.identity);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newBullet.GetComponent<Bullet>().dir = (mousePos - rb.position).normalized;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        movement = movement.normalized;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
