7590b76f4d62c1c26339b2c1f9f32c95ae0bd7bd
emilyk123 <emilykrohn@csu.fullerton.edu>
2022-11-10 22:01:12 -0800

    Added floor and boundaries for the room and player

--- Hells Kitchen Project/Assets/Scripts/CameraFollow.cs
+++ Hells Kitchen Project/Assets/Scripts/CameraFollow.cs
@@ -8,20 +8,35 @@ public class CameraFollow : MonoBehaviour
     public float rotationSpeed;
     private float rotateDir;
 
+    public BoxCollider2D boundBox;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
+
+    private void Start() {
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+    }
+
     private void Update()
     {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
         rotateDir = Input.GetAxisRaw("Rotate") * rotationSpeed;
 
-        Debug.Log(rotateDir);
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 
     private void FixedUpdate()
     {
         transform.Rotate(new Vector3(0, 0, rotateDir) * Time.fixedDeltaTime);
-    }
-
-    private void LateUpdate()
-    {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
     }
 }
--- Hells Kitchen Project/Assets/Scripts/Player.cs
+++ Hells Kitchen Project/Assets/Scripts/Player.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-
     private Animator anim;
-
     public float moveSpeed = 5f;
-
     private Vector2 movement;
-
     public GameObject bullet;
-
     public Enemy enemy;
-
     public float Health = 100f;
+
+    public BoxCollider2D boundBox;
+    private SpriteRenderer playerSprite;
+    private float playerWidth;
+    private float playerHeight;
+
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
 
     private void Awake()
     {
+        playerSprite = this.GetComponent<SpriteRenderer>();
         rb = this.GetComponent<Rigidbody2D>();
         anim = this.GetComponent<Animator>();
+
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        playerWidth = playerSprite.bounds.size.x / 2;
+        playerHeight = playerSprite.bounds.size.y / 2;
     }
 
     // Update is called once per frame
@@ -54,6 +63,10 @@ public class Player : MonoBehaviour
     {
         movement = movement.normalized;
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + playerWidth, maxBounds.x - playerWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + playerHeight, maxBounds.y - playerHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
